66432986c3ab411a37f3d2fda2cc0d12aaa356a1
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-23 01:10:55 +0900

    6.0~6.1_ useEffect : 딱 한번만 실행하고싶은 코드는 어떻게 해야할까?
    - useEffect를 사용하여 최초 렌더링 될때 한번만 실행하게 함: 처음 API를 불러올때 사용할 수 있음
    - useEffect(실행할 함수를 콜백함수로 작성,[]) ; 최초 렌더링 시에만 콜백함수가 실행됨

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,16 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter(prev => prev + 1);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...: i run only once. ");
+  }, []);
   return (
-    <div className="App">
-      <h1 className={styles.title}>WELCOME BACK</h1>
-      <Button text={"Continue"} />
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
