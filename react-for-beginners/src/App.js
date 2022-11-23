7ff114f27713b6200d921eb06c141eb7f5919103
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-23 19:23:49 +0900

    6.3_useEffect 복습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,13 +2,26 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter(prev => prev + 1);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
   console.log("i run all the time");
   useEffect(() => {
-    console.log("CALL THE API...: i run only once. ");
+    console.log("i run only once.");
   }, []);
+  useEffect(() => {
+    if (keyword.trim() !== "" && keyword.length > 5) {
+      console.log("I run when 'keyword' changes.");
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'counter' & 'keyword' change.");
+  }, [counter, keyword]);
   return (
     <div>
+      <input type="text" onChange={(e) => setKeyword(e.target.value)} />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
