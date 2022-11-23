b69af99bfdaa09f803bfed9be56dad40557b6360
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-23 19:48:52 +0900

    6.3_useEffect 복습 : useEffect를 통해 우리는 언제코드를 실행할 지 선택권을 가질 수 있다.

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,7 +4,9 @@ function App() {
   const [counter, setCounter] = useState(0);
   const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
   console.log("i run all the time");
+
   useEffect(() => {
     console.log("i run only once.");
   }, []);
@@ -21,7 +23,12 @@ function App() {
   }, [counter, keyword]);
   return (
     <div>
-      <input type="text" onChange={(e) => setKeyword(e.target.value)} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
