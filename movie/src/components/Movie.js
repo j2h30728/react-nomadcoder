30db96bec368f460ab6273562044f846d8c489a5
LeeJiHyeon <rachel2148072@gmail.com>
2022-11-29 19:31:00 +0900

    7.8

--- movie/src/components/Movie.js
+++ movie/src/components/Movie.js
@@ -7,7 +7,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 236 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
